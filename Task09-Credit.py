e78019ec09b3681c456f0ba1a4f3626cf1770f1b
ProgmanZ <dumphill@gmail.com>
2021-10-11 00:25:53 +0300

    [upd] финальный рабочий вариант.

--- Task09-Credit.py
+++ Task09-Credit.py
@@ -1,18 +1,23 @@
-def annuitentny_platezh(i, n, s):
-    # Вычисдяем по формуле из задачи
+def annuitentny_platezh(i, n, s):  # Функция вычисления аннуитентного платежа
     # i - процент
     # n - количество лет
     # s - сумма кредита
     k = i * (1 + i) ** n / ((1 + i) ** n - 1)
     a = k * s
+    a = round(a, 2)
     return a  # размер аннуитентного платежа
 
 
-def calc_body_credit(a, s, i):
+def calc_body_credit(a, s, i):  # Функция вычисления тела кредита
+    # a - аннуитентный платеж
+    # s - сумма кредита
+    # i - процент
     return a - (s * i)
 
 
-def viplachennye_percent(s, i):
+def viplachennye_percent(s, i):  # Функция вычисления выплаченного процента
+    # s - сумма кредита
+    # i - процент
     return s * i
 
 
@@ -20,16 +25,15 @@ s = float(input("Введите сумму кредита: "))
 n = float(input("На сколько лет выдан? "))
 i = float(input("Сколько процентов годовых? "))
 
-i /= 100
-a = annuitentny_platezh(i, n, s)
+i /= 100  # приведение процента в дробный вид
+a = annuitentny_platezh(i, n, s)  # вычисляем аннуитентный платеж до изменения условий
 
-period = 1
+period = 1  # переменная периода для печати и счетчика цикла
 vp = 0  # выплаченные проценты
-body = 0 # тело кредита
-count = 1
-flag = True
+body = 0  # тело кредита
+flag = True  # флаг показывающий изменение условий
 
-while count <= n:
+while period <= n:
     vp = viplachennye_percent(s, i)
     body = calc_body_credit(a, s, i)
 
@@ -41,19 +45,19 @@ while count <= n:
     s -= body
 
     if period == 3 and flag:
-        print("\nОстаток долга:",s)
+        print("\nОстаток долга:", s)
         print("\n ====================\n")
+
         n_2 = float(input("На сколько лет продляется договор? "))
         i_2 = float(input("Увеличение ставки до: "))
-        n += n_2 - period
-        i = i_2 / 100
-        a = annuitentny_platezh(i, n, s)
-        period = 0
-        flag = False
-        count = 0
 
-    count += 1
+        n += n_2 - period   # изменение условий по длительности лет
+        i = i_2 / 100   # перезапись новой процентной ставкой
+        a = annuitentny_platezh(i, n, s)    # вычисление размера аннуитентного платежа после изменения условий
+        period = 0
+
+        flag = False
+
     period += 1
 
 print("\nОстаток долга: ", s)
-
